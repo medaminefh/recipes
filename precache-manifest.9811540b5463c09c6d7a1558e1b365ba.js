bc87b9ff49e54d2d57582e4db877c00f41021f10
med-amine-fh <fhal.mohamed.amine@gmail.com>
2020-06-08 15:57:34 +0100

    Updates

---
+++ precache-manifest.9811540b5463c09c6d7a1558e1b365ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f842d1537976f267d01b1b62ff2d67be",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "2a5619e25e1c2718adb9",
+    "url": "/recipes/static/css/main.5c6a50f6.chunk.css"
+  },
+  {
+    "revision": "d2d917130659e4191a80",
+    "url": "/recipes/static/js/2.efffb5f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.efffb5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5619e25e1c2718adb9",
+    "url": "/recipes/static/js/main.cdbeef18.chunk.js"
+  },
+  {
+    "revision": "8505b6ea004b827d96b5",
+    "url": "/recipes/static/js/runtime-main.e431abca.js"
+  }
+]);
